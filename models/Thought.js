cc800f32a8d453545282ac1cb6b3c179fc38460a
Ignacio Diaz <nachodiaz99@gmail.com>
2022-01-01 14:49:12 +1100

    Thought schema done

--- models/Thought.js
+++ models/Thought.js
@@ -1,31 +1,24 @@
 const { Schema, model } = require('mongoose');
 
-// Schema to create a course model
-const courseSchema = new Schema(
+// Schema to create a thought model
+const thoughtSchema = new Schema(
   {
-    courseName: {
+    thoughtText: {
+      type: String,
+      required: true,
+      maxlength: 280,
+      minlength: 1,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      // Getter to format date timestamp TODO
+    },
+    username: {
       type: String,
       required: true,
     },
-    inPerson: {
-      type: Boolean,
-      default: true,
-    },
-    startDate: {
-      type: Date,
-      default: Date.now(),
-    },
-    endDate: {
-      type: Date,
-      // Sets a default value of 12 weeks from now
-      default: () => new Date(+new Date() + 84 * 24 * 60 * 60 * 1000),
-    },
-    students: [
-      {
-        type: Schema.Types.ObjectId,
-        ref: 'Student',
-      },
-    ],
+    reactions: [{ type: Schema.Types.ObjectId, ref: "Reaction" }],
   },
   {
     toJSON: {
@@ -35,6 +28,10 @@ const courseSchema = new Schema(
   }
 );
 
-const Course = model('course', courseSchema);
+thoughtSchema.virtual("reactionCount").get(function() {
+  return this.reactions.length;
+});
 
-module.exports = Course;
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought;
--- models/User.js
+++ models/User.js
@@ -1,7 +1,7 @@
 const { Schema, model } = require('mongoose');
 import { isEmail } from 'validator';
 
-// Schema to create Student model
+// Schema to create User model
 const userSchema = new Schema(
   {
     username: {
